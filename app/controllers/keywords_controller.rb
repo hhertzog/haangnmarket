4325e6a66f9791adcbf1beb97642d2ea409bdc0b
Hannah Hertzog <hhertzog@gmail.com>
2021-02-22 19:58:54 +0900

    editing new keyword form

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -3,7 +3,7 @@ class KeywordsController < ApplicationController
 
   # GET /keywords or /keywords.json
   def index
-    @keywords = Keyword.all
+    @keywords = Keyword.all.order("created_at DESC")
   end
 
   # GET /keywords/1 or /keywords/1.json
@@ -25,7 +25,7 @@ class KeywordsController < ApplicationController
 
     respond_to do |format|
       if @keyword.save
-        format.html { redirect_to @keyword, notice: "Keyword was successfully created." }
+        format.html { redirect_to keywords_url, notice: "Keyword was successfully created." }
         format.json { render :show, status: :created, location: @keyword }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class KeywordsController < ApplicationController
   def update
     respond_to do |format|
       if @keyword.update(keyword_params)
-        format.html { redirect_to @keyword, notice: "Keyword was successfully updated." }
+        format.html { redirect_to keywords_url, notice: "Keyword was successfully updated." }
         format.json { render :show, status: :ok, location: @keyword }
       else
         format.html { render :edit, status: :unprocessable_entity }
