86108de955c7b10ecfddc9cfac966f4a6ec7d145
Hannah Hertzog <hhertzog@gmail.com>
2021-03-04 13:23:10 +0900

    trying a keyword scope to find IDs of users with registered keywords

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,4 +2,7 @@ class Keyword < ApplicationRecord
   belongs_to :user
 
   validates :word, presence: true
+
+  # get all IDs of users with keywords registered
+  scope :users_with_keywords, -> { distinct.pluck(:user_id) }
 end
