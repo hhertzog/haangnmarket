94c9f68d469e9dd457bba5213acae4abe64897cc
Hannah Hertzog <hhertzog@gmail.com>
2021-03-06 23:51:48 +0900

    adding subscription callback to clean up unreferenced keywords

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,15 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :keyword
+
+  after_destroy :destroy_unreferenced_keywords
+
+  # TODO: put into a background job
+  def destroy_unreferenced_keywords
+  	keyword = Keyword.find(self.keyword_id)
+
+  	if keyword.users.empty?
+  		keyword.destroy
+  	end
+  end
 end
